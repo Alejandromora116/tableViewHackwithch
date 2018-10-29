3a244ebaadc344eca9af7c73f91ed9287d5f94d1
ALEJANDRO MORA <rl200042@rlas-macair-35.local>
2018-10-29 12:09:36 -0500

    Initial Commit

---
+++ tableViewHackwithch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tableViewHackwithch
+//
+//  Created by ALEJANDRO MORA on 10/29/18.
+//  Copyright © 2018 ALEJANDRO MORA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
